420754317d95adf4e64952f2eafdf07d385137a8
Thiago Oliveira Santos <tos.oliveira@gmail.com>
2018-01-27 01:44:35 -0200

    Changing test to convert a larger number of entities, for performance measurements

--- ConventionMapper.Test/MapperResolverTest.cs
+++ ConventionMapper.Test/MapperResolverTest.cs
@@ -2,6 +2,7 @@ using ConventionMapper.Test.Mirrors;
 using ConventionMapper.Test.Models;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using Xunit;
 
@@ -13,39 +14,38 @@ namespace ConventionMapper.Test
         public void Get_Success()
         {
             //Arrange
+            var itemCount = 1000000;
             Mapper.Load<MapperResolverTest>();
             var header = new HeaderTest()
             {
                 Id = Guid.NewGuid(),
                 StringField = "String value"
             };
-            var items = new List<ItemTest>()
-            {
-                new ItemTest()
+            var items = new List<ItemTest>();
+            for (var i = 0; i < itemCount; i++)
+                items.Add(new ItemTest()
                 {
                     Id = Guid.NewGuid(),
                     Header = header,
-                    StringField = "Item 1 String value"
-                },
-                new ItemTest()
-                {
-                    Id = Guid.NewGuid(),
-                    Header = header,
-                    StringField = "Item 2 String value"
-                }
-            };
+                    StringField = string.Format("Item {0} String value", i)
+                });
             header.Items = items;
 
             //Act
+            var moment = Stopwatch.StartNew();
             var headerMirror = Mapper.Map<HeaderTestMirror>(header);
-
+            moment.Stop();
             //Assert
             Assert.Equal(header.Id, headerMirror.Id);
             Assert.Equal(header.StringField, headerMirror.StringField);
-            Assert.Equal(header.Items.Count, headerMirror.Items.Count);
+            Assert.Equal(itemCount, headerMirror.Items.Count);
             var itemsMirror = headerMirror.Items.ToList();
-            AssertItem(items[0], headerMirror, itemsMirror[0]);
-            AssertItem(items[1], headerMirror, itemsMirror[1]);
+            for(var i = 0; i < itemCount; i++)
+                AssertItem(items[i], headerMirror, itemsMirror[i]);
+            //This test is subjective. In my machine I expect that the average elapsed time per converted
+            //entity to be less than 15 ticks, but it can fail depending on the host machine or in what
+            //is running at the moment
+            Assert.True(15 > moment.ElapsedTicks / (itemCount + 1));
         }
 
         private static void AssertItem(ItemTest item, HeaderTestMirror headerMirror, ItemTestMirror itemMirror)
